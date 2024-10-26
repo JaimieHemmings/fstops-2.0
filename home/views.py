5ff7626d9aeeadb8993454b765b362f860a7c130
Jaimie <jaimie.j.hemmings@gmail.com>
2024-10-26 17:20:36 +0100

    add: homepage sections

--- home/views.py
+++ home/views.py
@@ -6,3 +6,11 @@ def index(request):
   """
   context={}
   return render(request, 'home/index.html', context)
+
+
+def about(request):
+  """
+  A view that will render the index.html template
+  """
+  context={}
+  return render(request, 'about/about.html', context)
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   // Menu Toggle function
   let menuToggleBtn = document.getElementById('menu-burger');
   let menu = document.getElementById('nav-overlay');
@@ -42,5 +43,57 @@ document.addEventListener('DOMContentLoaded', function() {
 
     testimonials[currentTestimonial - 1].classList.remove('opacity-0');
     testimonials[currentTestimonial - 1].classList.remove('absolute');
+  }
+
+  // Add event listener to items with the class faq-toggle
+  let faqItems = document.querySelectorAll('.faq-toggle');
+
+  faqItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      let index = parseInt(item.getAttribute('data-index'));
+      toggleAccordion(index);
+    });
+  });
+
+  // Accordion Function
+  function toggleAccordion(index) {
+    // close all other accordion tabs
+    for (let i = 1; i <= faqItems.length; i++) {
+      if (i !== index) {
+        const content = document.getElementById(`content-${i}`);
+        const icon = document.getElementById(`icon-${i}`);
+        content.style.maxHeight = '0';
+        icon.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+            <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+          </svg>
+        `;
+      }
+    }
+    const content = document.getElementById(`content-${index}`);
+    const icon = document.getElementById(`icon-${index}`);
+ 
+    // SVG for Minus icon
+    const minusSVG = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+        <path d="M3.75 7.25a.75.75 0 0 0 0 1.5h8.5a.75.75 0 0 0 0-1.5h-8.5Z" />
+      </svg>
+    `;
+ 
+    // SVG for Plus icon
+    const plusSVG = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+        <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+      </svg>
+    `;
+ 
+    // Toggle the content's max-height for smooth opening and closing
+    if (content.style.maxHeight && content.style.maxHeight !== '0px') {
+      content.style.maxHeight = '0';
+      icon.innerHTML = plusSVG;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+      icon.innerHTML = minusSVG;
+    }
   }
 });
