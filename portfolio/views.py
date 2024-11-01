92adabdfa73d74bf1481a5f0e2a2f83cdef45644
Jaimie <jaimie.j.hemmings@gmail.com>
2024-11-01 12:33:54 +0000

    Update views.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator
 
 
 def portfolio(request):
-  paginator = Paginator(Portfolio.objects.all(), 6)
+  paginator = Paginator(Portfolio.objects.all().order_by("-date"), 6)
   page_num = request.GET.get("page")
   page_obj = paginator.get_page(page_num)
   context = {"page_obj": page_obj}
