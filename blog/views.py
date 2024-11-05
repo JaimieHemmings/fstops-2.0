a0eb90746ad330487d234340641a0a69d3320817
Jaimie <jaimie.j.hemmings@gmail.com>
2024-11-05 11:53:48 +0000

    add: date sorting to blog

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Article
 
 
 def blog(request):
-  paginator = Paginator(Article.objects.all(), 8)
+  paginator = Paginator(Article.objects.all().order_by("-date"), 8)
   page_num = request.GET.get("page")
   page_obj = paginator.get_page(page_num)
   context = {"page_obj": page_obj}
