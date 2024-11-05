691389d798a40dd49a4dfddb4f5573efae2d1ae9
Jaimie <jaimie.j.hemmings@gmail.com>
2024-11-05 12:13:18 +0000

    change: modified date generation in model

--- blog/models.py
+++ blog/models.py
@@ -3,43 +3,47 @@ from django.template.defaultfilters import slugify
 import uuid
 import string  # for string constants
 import random  # for generating random strings
+import datetime
 from ckeditor.fields import RichTextField
 
 
 class Article(models.Model):
-  def get_path(self, filename):
-    ext = filename.split('.')[-1]
-    filename = f"{uuid.uuid4()}.{ext}"
-    return f"articles/img/{filename}"
+    def get_path(self, filename):
+        ext = filename.split('.')[-1]
+        filename = f"{uuid.uuid4()}.{ext}"
+        return f"articles/img/{filename}"
   
-  title = models.CharField(max_length=120)
-  thumbnail = models.ImageField(upload_to=get_path)
-  slug = models.SlugField(null=True, blank=True, unique=True)
-  date = models.DateTimeField(auto_now_add=True)
-  intro = models.TextField(default="This is the intro")
-  lead_paragraph = models.TextField(default="This is the lead paragraph")
-  body = RichTextField()
-  body_img = models.ImageField(upload_to=get_path)
+    title = models.CharField(max_length=120)
+    thumbnail = models.ImageField(upload_to=get_path)
+    slug = models.SlugField(null=True, blank=True, unique=True)
+    date = models.DateField(("Date"), default=datetime.date.today)
+    intro = models.TextField(default="This is the intro")
+    lead_paragraph = models.TextField(default="This is the lead paragraph")
+    body = RichTextField()
+    body_img = models.ImageField(upload_to=get_path)
 
-  def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         self.slug = self.generate_slug()
         return super().save(*args, **kwargs)
   
-  def generate_slug(self, save_to_obj=False, add_random_suffix=True):
-      generated_slug = slugify(self.title)
+    def generate_slug(self, save_to_obj=False, add_random_suffix=True):
+        generated_slug = slugify(self.title)
       
-      random_suffix = ""
-      if add_random_suffix:
-          random_suffix = ''.join([
-              random.choice(string.ascii_letters + string.digits)
-              for i in range(5)
-          ])
-          generated_slug += '-%s' % random_suffix
+        random_suffix = ""
+        if add_random_suffix:
+            random_suffix = ''.join([
+                random.choice(string.ascii_letters + string.digits)
+                for i in range(5)
+            ])
+            generated_slug += '-%s' % random_suffix
 
-      if save_to_obj:
-          self.slug = generated_slug
-          self.save(update_fields=['slug'])
+        if save_to_obj:
+            self.slug = generated_slug
+            self.save(update_fields=['slug'])
         
-      return generated_slug
+        return generated_slug
+  
+def __str__(self):
+    return self.title
 
 
