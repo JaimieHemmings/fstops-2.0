e2686d36e8895905e22633819bb9e6f4cde16b89
Jaimie <jaimie.j.hemmings@gmail.com>
2024-11-07 14:19:00 +0000

    update: admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Article
 
+
 class ArticleAdmin(admin.ModelAdmin):
     search_fields = ["title", "content"]
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "date")
 
 admin.site.register(Article, ArticleAdmin)
